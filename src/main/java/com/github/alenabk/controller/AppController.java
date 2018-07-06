34a08363ab799d396f091848ac45fca5f0c737de
AlenaBK <alena@morko.ru>
2018-07-07 01:28:22 +0300

    initial commit

---
+++ src/main/java/com/github/alenabk/controller/AppController.java
@@ -0,0 +1,13 @@
+package com.github.alenabk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+
+    @RequestMapping("/")
+    public String helloPage() {
+        return "hello";
+    }
+}
